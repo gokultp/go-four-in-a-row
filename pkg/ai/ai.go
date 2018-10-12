6afd765a2a9d28f4b39b80bda9d8439425d64bed
berto <berto.ort@gmail.com>
2018-10-12 15:21:21 -0600

    Start board convertion to bitwise string

--- pkg/ai/ai.go
+++ pkg/ai/ai.go
@@ -1,7 +1,10 @@
 package ai
 
 import (
+	"strconv"
+
 	"github.com/gokultp/go-four-in-a-row/pkg/game"
+	"github.com/pkg/errors"
 )
 
 // C4AI represents the AI for connect four
@@ -10,4 +13,37 @@ type C4AI struct{}
 // MakeMove takes a game and returns a valie move
 func (ai *C4AI) MakeMove(g *game.Game) int {
 	return 0
+}
+
+func convertBoard(board [][]int) (int64, int64, error) {
+	position := ""
+	mask := ""
+
+	for _, row := range board {
+		for _, num := range row {
+			switch num {
+			case 1:
+				position += "1"
+				mask += "1"
+			case 2:
+				mask += "1"
+			default:
+				position += "0"
+			}
+		}
+	}
+
+	positionInt, err := strconv.ParseInt(position, 2, 64)
+
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "Failed to parse position bit")
+	}
+
+	maskInt, err := strconv.ParseInt(mask, 2, 64)
+
+	if err != nil {
+		return 0, 0, errors.Wrap(err, "Failed to parse position bit")
+	}
+
+	return positionInt, maskInt, nil
 }
--- pkg/ai/ai_test.go
+++ pkg/ai/ai_test.go
@@ -1,6 +1,7 @@
 package ai
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/gokultp/go-four-in-a-row/pkg/game"
@@ -22,5 +23,31 @@ func TestMakeMove(t *testing.T) {
 	// expect playerBeforeMove to not be the same player after move
 	if playerBeforeMove == playerAfterMove {
 		t.Fatalf("Invalid move: %v should not equal %v", playerBeforeMove, playerAfterMove)
+	}
+}
+
+func TestConvertBoard(t *testing.T) {
+	sampleBoard := [][]int{
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0, 0},
+		{0, 0, 0, 1, 2, 0, 0},
+		{0, 0, 0, 1, 1, 0, 0},
+		{0, 0, 2, 1, 2, 2, 0},
+	}
+
+	position, mask, err := convertBoard(sampleBoard)
+	if err != nil {
+		t.Fatalf("Failed to converboard with error: %s", err.Error())
+	}
+
+	expectedPosition, err := strconv.ParseInt("000000000000000000111000010100000000000000", 2, 64)
+	if err != nil || position != expectedPosition {
+		t.Fatalf("Invalid convertion: position %v should equal %v", position, expectedPosition)
+	}
+	expectedMask, err := strconv.ParseInt("000000000000100000111000111100100000000000", 2, 64)
+
+	if err != nil || mask != expectedMask {
+		t.Fatalf("Invalid convertion: mask %v should equal %v", mask, expectedMask)
 	}
 }
